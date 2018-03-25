42b41f4df88c5644e5f70b7ee2b19a96b31d6d37
Michael Chow <machow@princeton.edu>
2018-03-25 17:32:05 -0400

    hopefully first working copy

--- requirements.sh
+++ requirements.sh
@@ -1,10 +1,6 @@
 # If bash command fails, build should error out
 set -e
 
-# This bash script will be run on the Linux/Ubuntu image running your project.
-# Use it to install python packages necessary for your project.
-# and to setup the environment in other ways. 
-
-# For example, install specific package versions with pip 
-# pip3 install pandas==0.20.3
+# install dependencies
+pip3 install poetics==0.0.1
 
